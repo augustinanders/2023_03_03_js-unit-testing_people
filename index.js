db3e7e141c4eb1fbc1f1ed98c24deeffb84d64a3
Augustin Anders <augustinanders@gmail.com>
2023-03-06 09:20:51 +0100

    my solution

--- index.js
+++ index.js
@@ -1,19 +1,58 @@
 export function getFirstNames(people) {
-  return people.map((person) => person.firstName);
+  return people.map((person) => {
+    return person.firstName;
+  });
 }
 
-export function getFullNames(people) {}
+export function getFullNames(people) {
+  return people.map((person) => {
+    return `${person.firstName} ${person.lastName}`;
+  });
+}
 
-export function getNameAndAge(people) {}
+export function getNameAndAge(people) {
+  return people.map((person) => {
+    return `${person.firstName} (${person.age})`;
+  });
+}
 
-export function getPeopleByAge(people, age) {}
+export function getPeopleByAge(people, age) {
+  return people.filter((person) => {
+    return person.age === age;
+  });
+}
 
-export function getPeopleNamesOlderThan(people, age) {}
+export function getPeopleNamesOlderThan(people, age) {
+  return people
+    .filter((person) => {
+      return person.age > age;
+    })
+    .map((person) => {
+      return `${person.firstName} ${person.lastName}`;
+    });
+}
 
-export function getPeopleByLastName(people, lastName) {}
+export function getPeopleByLastName(people, lastName) {
+  return people.filter((person) => {
+    return person.lastName === lastName;
+  });
+}
 
-export function findPersonById(people, id) {}
+export function findPersonById(people, id) {
+  return people.find((person) => {
+    return person.id === id;
+  });
+}
 
-export function isAnyoneOlderThan(people, age) {}
+export function isAnyoneOlderThan(people, age) {
+  return people.some((person) => {
+    return person.age > age;
+  });
+}
 
-export function getFullNamesSortedByAge(people) {}
+export function getFullNamesSortedByAge(people) {
+  return people
+    .slice()
+    .sort((a, b) => a.age - b.age)
+    .map((person) => `${person.lastName}`);
+}
--- index.test.js
+++ index.test.js
@@ -48,22 +48,92 @@ test("getFirstNames() returns an array with the first names", () => {
   expect(result).toEqual(["Jane", "John", "Max", "Erika", "Luke"]);
 });
 
-test('getFullNames() returns an array with full names as strings, e.g. "Jane Doe"', () => {});
+test('getFullNames() returns an array with full names as strings, e.g. "Jane Doe"', () => {
+  const result = getFullNames(people);
+  expect(result).toEqual([
+    "Jane Doe",
+    "John Doe",
+    "Max Mustermann",
+    "Erika Musterfrau",
+    "Luke Skywalker",
+  ]);
+});
 
-test('getNameAndAge() returns an array of formatted strings like this: "Skywalker (23)"', () => {});
+test('getNameAndAge() returns an array of formatted strings like this: "Skywalker (23)"', () => {
+  const result = getNameAndAge(people);
+  expect(result).toEqual([
+    "Jane (21)",
+    "John (18)",
+    "Max (32)",
+    "Erika (38)",
+    "Luke (23)",
+  ]);
+});
 
-test("getPeopleByAge() returns an array of people of a certain age", () => {});
+test("getPeopleByAge() returns an array of people of a certain age", () => {
+  const result = getPeopleByAge(people, 38);
+  expect(result).toEqual([
+    {
+      id: 4,
+      firstName: "Erika",
+      lastName: "Musterfrau",
+      age: 38,
+    },
+  ]);
+});
 
-test('getPeopleNamesOlderThan() returns an array of people full names older than a certain age, e.g. "Luke Skywalker"', () => {});
+test('getPeopleNamesOlderThan() returns an array of people full names older than a certain age, e.g. "Luke Skywalker"', () => {
+  const result = getPeopleNamesOlderThan(people, 23);
+  expect(result).toEqual(["Max Mustermann", "Erika Musterfrau"]);
+});
 
-test("getPeopleByLastName() returns an array of people of a certain lastName", () => {});
+test("getPeopleByLastName() returns an array of people of a certain lastName", () => {
+  const result = getPeopleByLastName(people, "Doe");
+  expect(result).toEqual([
+    {
+      id: 1,
+      firstName: "Jane",
+      lastName: "Doe",
+      age: 21,
+    },
+    {
+      id: 2,
+      firstName: "John",
+      lastName: "Doe",
+      age: 18,
+    },
+  ]);
+});
 
 // Hint: use Array.prototype.find for the implementation
-test("getPersonById() returns the full object of a person", () => {});
+test("findPersonById() returns the full object of a person", () => {
+  const result = findPersonById(people, 2);
+  expect(result).toEqual({
+    id: 2,
+    firstName: "John",
+    lastName: "Doe",
+    age: 18,
+  });
+});
 
 // Hint: implementation should use Array.prototype.some
-test("isAnyoneOlderThan() returns true if any person is older than the given age", () => {});
+test("isAnyoneOlderThan() returns true if any person is older than the given age", () => {
+  const result = isAnyoneOlderThan(people, 32);
+  expect(result).toBe(true);
+});
 
-test("isAnyoneOlderThan() returns false if no person is older than the given age", () => {});
+test("isAnyoneOlderThan() returns false if no person is older than the given age", () => {
+  const result = isAnyoneOlderThan(people, 38);
+  expect(result).toBe(false);
+});
 
-test("getFullNamesSortedByAge() returns an array of last names sorted by age", () => {});
+test("getFullNamesSortedByAge() returns an array of last names sorted by age", () => {
+  const result = getFullNamesSortedByAge(people);
+  expect(result).toEqual([
+    "Doe",
+    "Doe",
+    "Skywalker",
+    "Mustermann",
+    "Musterfrau",
+  ]);
+});
